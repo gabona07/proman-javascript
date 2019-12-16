d712a2f405a4b382b483a7cb4239d8a1a8336a6d
gabona07 <bonagabriella1995@gmail.com>
2019-12-16 13:27:33 +0100

    Add basic values for tables

--- data/proman.sql
+++ data/proman.sql
@@ -15,13 +15,13 @@ DROP TABLE IF EXISTS statuses;
 CREATE TABLE boards (
     id serial PRIMARY KEY,
     title text,
-    user_id serial
+    user_id integer
 );
 
 CREATE TABLE cards (
     id serial PRIMARY KEY,
-    title text,
     board_id serial,
+    title text,
     status_id serial,
     card_order serial
 );
@@ -39,8 +39,6 @@ CREATE TABLE users (
     password text
 );
 
-
-
 ALTER TABLE cards
     ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id)
           REFERENCES boards(id) ON DELETE CASCADE;
@@ -52,3 +50,29 @@ ALTER TABLE cards
 ALTER TABLE boards
     ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id)
           REFERENCES users(id) ON DELETE CASCADE;
+
+
+INSERT INTO boards VALUES (1,'Board 1');
+INSERT INTO boards VALUES (2,'Board 2');
+
+INSERT INTO statuses VALUES (0, 'new');
+INSERT INTO statuses VALUES (1, 'in progress');
+INSERT INTO statuses VALUES (2, 'testing');
+INSERT INTO statuses VALUES (3, 'done');
+
+INSERT INTO cards VALUES (1,1,'new card 1',0,0);
+INSERT INTO cards VALUES (2,1,'new card 2',0,1);
+INSERT INTO cards VALUES (3,1,'in progress card',1,0);
+INSERT INTO cards VALUES (4,1,'planning',2,0);
+INSERT INTO cards VALUES (5,1,'done card 1',3,0);
+INSERT INTO cards VALUES (6,1,'done card 1',3,1);
+INSERT INTO cards VALUES (7,2,'new card 1',0,0);
+INSERT INTO cards VALUES (8,2,'new card 2',0,1);
+INSERT INTO cards VALUES (9,2,'in progress card',1,0);
+INSERT INTO cards VALUES (10,2,'planning',2,0);
+INSERT INTO cards VALUES (11,2,'done card 1',3,0);
+INSERT INTO cards VALUES (12,2,'done card 1',3,1);
+
+
+
+
