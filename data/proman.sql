f4a5c56468cb4b7bf5da2b2a245505d2b8f37fdc
gabona07 <bonagabriella1995@gmail.com>
2019-12-16 13:00:44 +0100

    Add user table

--- data/proman.sql
+++ data/proman.sql
@@ -1,10 +1,12 @@
 ALTER TABLE IF EXISTS ONLY public.boards DROP CONSTRAINT IF EXISTS boards_pkey CASCADE;
 ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS cards_pkey CASCADE;
 ALTER TABLE IF EXISTS ONLY public.statuses DROP CONSTRAINT IF EXISTS statuses_pkey CASCADE;
+ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS users_pkey CASCADE;
+ALTER TABLE IF EXISTS ONLY public.boards DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_board_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.cards DROP CONSTRAINT IF EXISTS fk_status_id CASCADE;
 
-
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS boards;
 DROP TABLE IF EXISTS cards;
 DROP TABLE IF EXISTS statuses;
@@ -12,7 +14,8 @@ DROP TABLE IF EXISTS statuses;
 
 CREATE TABLE boards (
     id serial PRIMARY KEY,
-    title text
+    title text,
+    user_id serial
 );
 
 CREATE TABLE cards (
@@ -29,6 +32,15 @@ CREATE TABLE statuses (
 );
 
 
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    title text,
+    username text,
+    password text
+);
+
+
+
 ALTER TABLE cards
     ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id)
           REFERENCES boards(id) ON DELETE CASCADE;
@@ -36,3 +48,7 @@ ALTER TABLE cards
 ALTER TABLE cards
     ADD CONSTRAINT fk_status_id FOREIGN KEY (status_id)
           REFERENCES statuses(id) ON DELETE CASCADE;
+
+ALTER TABLE boards
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id)
+          REFERENCES users(id) ON DELETE CASCADE;
