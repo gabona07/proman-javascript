88542a2b62c966cf6af94229f9bce01265aa6a85
zsoltion-cc <55063936+zsoltion-cc@users.noreply.github.com>
2019-12-19 13:46:27 +0100

    Fix PEP8

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-from util import json_response, hash_password, verify_password
+from util import json_response, hash_password
 
 import data_handler
 
